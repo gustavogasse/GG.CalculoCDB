78801b3835a0390964a0e3e482976c0c5f119d2a
Gustavo Gassenferth <gustavo.gasse@gmail.com>
2022-12-19 22:50:57 -0300

    Exclusão referencias

--- GG.CalculoCDB.Test/CalculoImpostoServiceTest.cs
+++ GG.CalculoCDB.Test/CalculoImpostoServiceTest.cs
@@ -2,12 +2,6 @@
 using GG.CalculoCDB.Domain.Repositories;
 using GG.CalculoCDB.Domain.Services;
 using GG.CalculoCDB.UI.Web.Services;
-using GG.CalculoCDB.UI.Web.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GG.CalculoCDB.Test
 {
